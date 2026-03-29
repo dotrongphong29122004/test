ee36f190ae6529798761c3e60bc314ed05fe9290
dotrongphong29122004 <dotrongphong29122004@gmail.com>
2026-03-29 20:22:20 +0700

    Delete Backend directory

--- Backend/src/routes/category.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const categoryController = require('../controllers/categoryController');
-
-router.get('/', categoryController.getAll);
-
-module.exports = router;
